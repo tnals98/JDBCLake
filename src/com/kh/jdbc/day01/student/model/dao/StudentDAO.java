ec280682615530af781de50c323503f8c2b76c5d
tnals98 <sumin090598@gmail.com>
2023-07-20 17:18:15 +0900

    [INIT] 23/07/20 JDBC 프로그래밍 개념 및 실습
    1. JDBC 드라이버, ojdbc6.jar ADD 중요
     - Class.forName()
    2. JDBC 코딩절차에 따른 Class, Interface 사용
     - DriverManager, Connection, Statement, ResultSet(SELECT)
    3. 후처리의 중요성
     - next(), getString(), getInt(), getDate(),...
     4. 학생관리 프로그램 MVC 패턴 적용 후 JDBC 프로그래밍 완료(학생 전체정보 조회)

---
+++ src/com/kh/jdbc/day01/student/model/dao/StudentDAO.java
@@ -0,0 +1,66 @@
+package com.kh.jdbc.day01.student.model.dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kh.jdbc.day01.student.model.vo.Student;
+
+public class StudentDAO {
+	
+	public List<Student> selectAll() {
+		/*
+		 * 1. 드라이버 등록
+		 * 2. DB 연결 생성
+		 * 3. 쿼리문 실행 준비
+		 * 4. 쿼리문 실행 및 5. 결과 받기
+		 * 6. 자원해제(close())
+		 */
+		String driverName = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String user = "STUDENT";
+		String password = "STUDENT";
+		String query = "SELECT *FROM STUDENT_TBL";
+		List<Student> sList = null;
+		Student student = null;
+		try {
+			// 1. 드라이버 등록
+			Class.forName(driverName);
+			// 2. DB 연결 생성(DriverManager)
+			Connection conn = DriverManager.getConnection(url, user, password);
+			// 3. 쿼리문 실행 준비
+			Statement stmt = conn.createStatement();
+			// 4. 쿼리문 실행 및 5. 결과 받기
+			ResultSet rset = stmt.executeQuery(query);
+			sList = new ArrayList<Student>();
+			// 후처리
+			while(rset.next()) {
+				student = new Student();
+				student.setStudentId(rset.getString("STUDENT_ID"));
+				student.setStudentPwd(rset.getString("STUDENT_PWD"));
+				student.setStudentName(rset.getString("STUDENT_NAME"));
+				student.setAge(rset.getInt("AGE"));
+				student.setEmail(rset.getString("EMAIL"));
+				student.setPhone(rset.getString("PHONE"));
+				// 문자는 문자열에 문자로 잘라서 사용, charAt() 메소드 사용
+				student.setGender(rset.getString("GENDER").charAt(0));
+				student.setAddress(rset.getString("ADDRESS"));
+				student.setHobby(rset.getString("HOBBY"));
+				student.setEnrollDate(rset.getDate("ENROLL_DATE"));
+				sList.add(student);
+//				System.out.printf("아이디 : %s, 이름 : %s\n", rset.getString("STUDENT_ID"), rset.getString("STUDENT_NAME"));
+			}
+			// 6. 자원해제
+			rset.close();
+			stmt.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return sList;
+	}
+}
